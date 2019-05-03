6b5d8274e83e06e51899e03f54e78d4c76f8f772
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-05-03 23:01:55 +0800

    Merge branch 'feature/broker' into develop

--- src/main/java/com/example/trader/Util/BrokerWrapperFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.trader.Util;
-
-import com.example.trader.Util.Wrapper.BrokerWrapper;
-
-public class BrokerWrapperFactory {
-    public static BrokerWrapper create(String url){
-        return new BrokerWrapper(url);
-    }
-}
--- src/main/java/com/example/trader/Util/Wrapper/BrokerWrapper.java
+++ src/main/java/com/example/trader/Util/Wrapper/BrokerWrapper.java
@@ -3,6 +3,7 @@ package com.example.trader.Util.Wrapper;
 public class BrokerWrapper {
     private int id;
     private String url;
+    private String description;
 
     public String getUrl() {
         return url;
@@ -13,16 +14,20 @@ public class BrokerWrapper {
     }
 
     public BrokerWrapper(){}
-
-    public BrokerWrapper(String url){
-        this.url = url;
-    }
-
+    
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
