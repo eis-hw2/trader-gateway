920d72ca1c4561612462a5be0a3f0e72852e1b39
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-05-28 17:27:05 +0800

    small fix

--- src/main/java/com/example/trader/Core/Processor/Strategy/TwapProcessor.java
+++ src/main/java/com/example/trader/Core/Processor/Strategy/TwapProcessor.java
@@ -1,15 +1,36 @@
 package com.example.trader.Core.Processor.Strategy;
 
-import com.example.trader.Domain.Entity.Order;
 import com.example.trader.Core.Processor.Processor;
+import com.example.trader.Domain.Entity.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class TwapProcessor extends Processor {
+public class TwapProcessor extends Processor{
+
+    private final int DEFAULT_NUMBER = 10;
+    private int slice = DEFAULT_NUMBER;
+
     @Override
-    public List<Order> process(Order order){
-        return null;
+    public List<Order> process(Order order) {
+        int total = order.getCount();
+        int mean = total / getSlice();
+        List<Order> orders = new ArrayList<>();
+        for (int i = 0; i < getSlice(); i++){
+            Order o = new Order(order);
+            o.setCount(mean);
+            orders.add(o);
+        }
+        return orders;
+    }
+
+    public int getSlice() {
+        return slice;
+    }
+
+    public void setSlice(int slice) {
+        this.slice = slice;
     }
 }
